f0595e0d43f1e5d116948c1c75809f409302a43d
adityasunny1189 <adityapathak1189@gmail.com>
2020-04-29 01:30:01 +0530

    Adding Css styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './styles.css';
 
 function App() {
 	return (
-		<h1>Hello React Again</h1>
+		<h1 className = "first">Hello React Welcome to Styles Tag</h1>
 	)
 }
 
